5eb1020ead7374cfcfe08e5be1be16ff45b842ff
Konstantin <goodluckhf@yandex.ru>
2018-07-23 12:09:22 +0300

    feat(src/Config.js): Добавил обьект Конфига с тестами

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,38 @@
 class Config {
+	constructor(targets = []) {
+		this.targets = [];
+		
+		if (!Array.isArray(targets)) {
+			return this.addTarget(targets);
+		}
+		
+		targets.forEach((target) => {
+			this.addTarget(target);
+		});
+	}
+	
+	/**
+	 * @param {string} target
+	 * @returns {Config}
+	 */
+	addTarget(target) {
+		this.targets.push(target);
+		return this;
+	}
+	
+	/**
+	 * @returns {string}
+	 */
+	toString() {
+		return JSON.stringify([{
+			targets: this.targets,
+		}]);
+	}
+	
+	/**
+	 * @param {Array|string} opts
+	 * @returns {Config}
+	 */
 	static create(opts) {
 		return new this(opts);
 	}
---
+++ test/config.test.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import Config     from '../src/Config';
+
+describe('Config', () => {
+	it('Possible to create from list of targets', () => {
+		const targetsList = ['test1'];
+		const config = Config.create(targetsList);
+		expect(config.targets[0]).to.be.equal(targetsList[0]);
+	});
+	
+	it('Possible to create from one target', () => {
+		const target = 'newTarget';
+		const config = Config.create(target);
+		expect(config.targets[0]).to.be.equal(target);
+	});
+	
+	it('Possible to add target to exist config object', () => {
+		const newTarget = 'testTarget';
+		const config = new Config();
+		config.addTarget(newTarget);
+		expect(config.targets[0]).to.be.equal(newTarget);
+	});
+	
+	it('Config object should be converted to string for prometheus config', () => {
+		const newTarget = 'testTarget';
+		const config = Config.create(newTarget);
+		const expectedValue = JSON.stringify([{ targets: [newTarget] }]);
+		expect(config.toString()).to.be.equal(expectedValue);
+	});
+});
